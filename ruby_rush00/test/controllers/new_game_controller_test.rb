7ab34c832c096823bf0769fc01958af1ffbb3183
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-07-31 17:58:25 +0900

    delete

--- ruby_rush00/test/controllers/new_game_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class NewGameControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- ruby_rush00/test/controllers/power_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PowerControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
