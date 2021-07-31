15d5e429a048f4ff6f7ba17fe0ca0adf4d14d48d
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-07-31 17:57:50 +0900

    temp

--- moviemon/urls.py
+++ moviemon/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
 from . import views
+
+urlpatterns = [
+    path('title/', views.title, name='title')
+]
--- moviemon/views.py
+++ moviemon/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def title(request):
+    return render(request, 'title.html')
